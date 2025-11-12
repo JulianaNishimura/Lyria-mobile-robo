9930b5fefca8e3a02517d9f1d4e4653169588742
JulianaNishimura <juliana.nishimura.tds24@gmail.com>
2025-11-12 15:00:43 -0300

    tentar deixar mais alto

--- LyriaMobile/App.js
+++ LyriaMobile/App.js
@@ -82,9 +82,9 @@ export default function App() {
     if ('speechSynthesis' in window) {
       const utterance = new SpeechSynthesisUtterance(text);
       utterance.lang = 'pt-BR';
-      utterance.pitch = 1;
-      utterance.rate = 1;
-      utterance.volume = 1;
+      utterance.pitch = 1.05;
+      utterance.rate = 0.95;
+      utterance.volume = 1.0;
       speechSynthesis.cancel(); // Interrompe falas anteriores
       speechSynthesis.speak(utterance);
     } else {
